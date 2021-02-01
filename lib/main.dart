b4e8d2bfc888c767583d008de053641567b29481
imark00 <marksonline247@gmail.com>
2021-02-01 17:47:55 +0000

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(StartApp());
 }
 
-class MyApp extends StatelessWidget {
+class StartApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
